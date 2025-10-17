1affc5647b28f24b13eb3c5d5d7e1daeeaf2463d
data-team <michaelseavers@hotmail.com>
2025-10-17 14:01:31 -0500

    insertions

--- server/app/main.py
+++ server/app/main.py
@@ -9,13 +9,8 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi_mqtt import FastMQTT, MQTTConfig
 import os
+from .database import Database
 from typing import List
-
-# mqtt_config = MQTTConfig(
-#     host=os.getenv("MQTT_HOST", "localhost"),
-#     port=int(os.getenv("MQTT_PORT", 1883)),
-#     keepalive=60,
-# )
 
 mqtt_config = MQTTConfig(
     host="host.docker.internal",
@@ -27,65 +22,61 @@ app = FastAPI()
 mqtt = FastMQTT(config=mqtt_config)
 mqtt.init_app(app)
 
-messages: List[str] = []
-
 # Test ping if server is running and recieving
 @app.get("/ping")
 def ping():
-    return {"status": "ok"}
+  return {"status": "ok"}
 
-@app.get("/", response_class=HTMLResponse)
-def index():
-    # Simple HTML page with JS to fetch messages and display
-    return """
-    <!DOCTYPE html>
-    <html>
-    <head><title>MQTT Messages</title></head>
-    <body>
-      <h1>MQTT Messages</h1>
-      <ul id="messages"></ul>
+@app.get("/session/start")
+def start_session():
 
-      <script>
-        async function fetchMessages() {
-          const res = await fetch('/messages');
-          const data = await res.json();
-          const ul = document.getElementById('messages');
-          ul.innerHTML = '';
-          data.forEach(msg => {
-            const li = document.createElement('li');
-            li.textContent = msg;
-            ul.appendChild(li);
-          });
-        }
+  try:
+    db: Database = app.state.db
 
-        // Fetch messages every 2 seconds
-        setInterval(fetchMessages, 2000);
-        fetchMessages();
-      </script>
-    </body>
-    </html>
-    """
+    await db.create_session (label=device_id)
 
-@app.get("/messages")
-def get_messages():
-    # Return the list of messages as JSON
-    return messages
+  except Exception as e:
+    print(f"DB insert failed for topic={topic}: {e}", flush=True)
 
+@app.get("/session/stop")
+def stop_session():
+  pass
+
+@app.on_event("startup")
+async def startup():
+    app.state.db = await Database.create()
+
+@mqtt.subscribe("robot/#")
+async def handle_sensors(client, topic, payload, qos, prop):
+  msg = f"Sensor ({topic}): {payload.decode()}"
+  print(msg)
 
 @mqtt.subscribe("sensor/#")
 async def handle_sensors(client, topic, payload, qos, prop):
-    msg = f"Sensor ({topic}): {payload.decode()}"
-    print(msg)
-    messages.append(msg)
+  msg = f"Sensor ({topic}): {payload.decode()}"
+  print(msg)
 
 @mqtt.subscribe("camera/#")
 async def handle_camera(client, topic, payload, qos, prop):
-    msg = f"Camera ({topic}): {payload.decode()} [{client}]"
-    print(msg)
-    messages.append(msg)
+
+  device_id = topic.split("/")[1]
+  msg = payload.decode()
+
+  print(msg, flush=True)
+
+  try:
+    db: Database = app.state.db
+
+    await db.insert_device(
+        label=device_id,
+        category="camera",
+        ip_address="0.0.0.0",
+        )
+
+  except Exception as e:
+    print(f"DB insert failed for topic={topic}: {e}", flush=True)
 
 @mqtt.on_message()
 async def message(client, topic, payload, qos, prop):
-    msg = f"Message from device ({topic}): {payload.decode()}"
-    print(msg)
-    messages.append(msg)
+  msg = f"Message from device ({topic}): {payload.decode()}"
+  print(msg)
