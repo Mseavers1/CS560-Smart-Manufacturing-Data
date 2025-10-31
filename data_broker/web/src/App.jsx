9fd4fad2b5c6f0e9672c6198ee637738b0e6bd9c
cylissama <cydaniel@ymail.com>
2025-10-31 13:39:19 -0500

    Topbar

--- data_broker/web/src/App.jsx
+++ data_broker/web/src/App.jsx
@@ -1,34 +1,34 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import HomePage from './Home.jsx';
+import DataDashboard from './DataDashboard.jsx'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <BrowserRouter>
+
+      {/* Topbar */}
+      <nav className="navbar">
+        <h2 className="title">Smart Manufacturing Research</h2>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/data/dashboard">Data Dashboard</Link>
+        </div>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/data/dashboard" element={<DataDashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ data_broker/web/src/DataDashboard.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+export default function DataDashboard() {
+
+    return (
+        <div>
+            <h1> Data Dashboard </h1>
+        </div>
+    )
+
+
+}
